2f0044b631db21308359e120887c09f1d7559ba1
Lucas Pinheiro <lucaspinheiro.almeyda@gmail.com>
2026-06-23 00:44:34 -0300

    all class

--- src/data/links.ts
+++ src/data/links.ts
@@ -30,6 +30,41 @@ export const categories: LinkCategory[] = [
         title: 'Aula 2,2',
         url: 'https://drive.google.com/file/d/1kqKK6IlPNFuMJa30GUvgTkqHwJp6ksXW/view?usp=drive_link',
       },
+      {
+        id: 5,
+        title: 'Aula 3,1',
+        url: 'https://drive.google.com/file/d/1ycw3tNZxKkfd4jpuOo0ppcpzttACEktF/view?usp=drive_link',
+      },
+      {
+        id: 6,
+        title: 'Aula 3,2',
+        url: 'https://drive.google.com/file/d/14AgaUYDlhjFUzZBvpYL2olo1U-3dVfZv/view?usp=drive_link',
+      },
+      {
+        id: 7,
+        title: 'Aula 4,1',
+        url: 'https://drive.google.com/file/d/1IMoGOlhm86r5Pr8vLIYBtDkh5VqfH9od/view?usp=drive_link',
+      },
+      {
+        id: 8,
+        title: 'Aula 4,2',
+        url: 'https://drive.google.com/file/d/1i1TP1UXhuAAT_AW0jhAyD9cXHdsX3oqK/view?usp=drive_link',
+      },
+      {
+        id: 9,
+        title: 'Aula 5,1',
+        url: 'https://drive.google.com/file/d/1-98Ky9I4FxFF7FeznItuX5WUq3hbUadz/view?usp=drive_link',
+      },
+      {
+        id: 10,
+        title: 'Aula 5,2',
+        url: 'https://drive.google.com/file/d/1yw2CbqZcapz8GIPg-9YyOmDVERv2zFRS/view?usp=drive_link',
+      },
+      {
+        id: 10,
+        title: 'Aula 5,3',
+        url: 'https://drive.google.com/file/d/1jXD99AORRygI0PNs5O_yZDYeDviIoPPd/view?usp=drive_link',
+      },
     ],
   },
   {
@@ -50,6 +85,16 @@ export const categories: LinkCategory[] = [
         id: 3,
         title: 'Digestivo',
         url: 'https://share.gemini.google/vrCAamDeinqf',
+      },
+      {
+        id: 4,
+        title: 'Laringe y Faringe',
+        url: 'https://share.gemini.google/8S7WXBTGZASC',
+      },
+      {
+        id: 5,
+        title: 'Gemitourinario',
+        url: 'https://share.gemini.google/WaZBwDxXb2XW',
       },
     ],
   },
